693849e32e3d165de7749437abef46c5e7640982
Eric C. Anderson <eric.anderson@noaa.gov>
2015-12-05 22:03:10 -0800

    Meta data summaries and reorganized Rnw file
    * Started writing up the meta data section of the paper and it was clear I needed to make a *sample_sheet* with the names of all the samples that we are going to use in the analyses.
    * Also, inserted all the main-body-text into the main Rnw file so that PDF sync will work.

--- R-main/00-meta-data-summaries.R
+++ R-main/00-meta-data-summaries.R
@@ -1,4 +1,4 @@
-
+library(dplyr)
 
 # read in the full repository information:
 repo <- tbl_df(read.table("data/meta/AM001_AM006.tab", sep = "\t", header = T, stringsAsFactors = FALSE))
@@ -6,7 +6,40 @@ repo <- tbl_df(read.table("data/meta/AM001_AM006.tab", sep = "\t", header = T, s
 # read in the bycatch IDs info:
 bycid <- readRDS("data/meta/bycatch_IDS.rds")
 
+# read in the sheet of data about the samples we are using
+samsheet <- read.csv("data/meta/sample_sheet.csv", stringsAsFactors = FALSE)
 
+
+# so, in the future, we can left_join on samsheet to get a consistent set of 
+# individuals to use.
+
+#### Here is how I created sample categories and made a sample list.  Doesn't get re-run... ####
+if(FALSE) {  # this doesn't typically get re-run because I made the file to use in the future
+  sams <- repo %>% 
+    filter(!(NMFS_DNA_ID %in% bycid$Duplicate_Tissue)) %>%  # toss out one of the duplicates of each duplicated one
+    filter(!(NMFS_DNA_ID %in% c("AM000193", "AM000260", "AM000263"))) %>%   # bycatch individuals without proper key back to the Region's data base
+    filter(NMFS_DNA_ID != "AM000474") %>%    # individual for which proper meta data are not available
+    mutate(
+      category = ifelse(WATERSHED %in% c("Sacramento River", "Klamath River"), 
+                        "reference", "non-reference"),
+      life_stage = ifelse(toupper(REPORTED_LIFE_STAGE) == "JUVENILE",
+                          "juvenile", ifelse(str_detect(SAMPLE_COMMENTS, "^GST"), "egg", "non-juvenile")),
+      collection_location = ifelse(NMFS_DNA_ID %in% bycid$NMFS_DNA_ID, "Bycatch", WATERSHED)
+      ) %>% 
+    select(NMFS_DNA_ID, category, life_stage, collection_location) %>%
+    mutate(group_name = paste(
+      str_sub(category, 1, 1), 
+      str_sub(life_stage, 1, 1), 
+      str_sub(collection_location, 1, 3),
+      sep = ""
+      )) %>%
+    mutate(pipe_name = paste(group_name, str_sub(NMFS_DNA_ID, 6, 8), sep = ""))
+  
+  # write that out 
+  write.csv(sams, file = "data/meta/sample_sheet.csv")
+}
+
+if(FALSE) {
 #### Summarize the Bycatch Data ####
 # filter out things that aren't in the repo (there aren't any)
 byc <- bycid %>%
@@ -30,17 +63,24 @@ write.table(tmp, sep = " & ", eol = "\\\\\n",
             row.names = F, col.names = F, file = outf, 
             quote = FALSE, append = TRUE)
 
+}  # took this out for data privacy reasons.  Code here if we want to go
+                 # back to make such a table
+
+#### Make a table of sample counts ####
+
+stab <- samsheet %>%
+  mutate(category = factor(category, levels = rev(unique(category)))) %>%
+  mutate(collection_location = factor(collection_location, levels = rev(sort(unique(collection_location))))) %>%
+  group_by(category, life_stage, collection_location, group_name) %>%
+  tally() %>%
+  setNames(c("Category", "Life Stage", "Location", "Group Short Name", "n"))
 
 
-#### Now summarize the freshwater samples ####
-
-# toss out the ones we know are bycatch
-nonbyc <- repo %>%
-  filter(!(NMFS_DNA_ID %in% byc$NMFS_DNA_ID), !(NMFS_DNA_ID %in% byc$Duplicate_Tissue_Of))
-         
-         
-         
-         
+outf <- "outputs/sample_summary.tex"
+cat(names(stab), sep = " & ", file = outf)
+cat("\\\\ \\hline\n", file = outf, append = TRUE)
+write.table(stab, sep = " & ", eol = "\\\\\n", col.names = F, 
+            row.names = F, quote = F, file = outf, append = T)
          
          
          
